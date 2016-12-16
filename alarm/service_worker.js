7985991217af1d6fb66e9db0cb88bd3442e0250f
Nikolay Fedoseev <xneek@mail.ru>
2016-12-16 20:41:01 +0300

    test alarm project

--- alarm/service_worker.js
+++ alarm/service_worker.js
@@ -1,4 +1,6 @@
-    this.ontask = function(task) {
+   
+console.log('SW');
+this.ontask = function(task) {
         alert(task.data.message);
         console.log("Task scheduled at: " + new Date(task.time));
         // From here on we can write the data to IndexedDB, send it to any open windows,

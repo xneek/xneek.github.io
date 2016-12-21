32cc2b767540b90ca830aed1009e2093063c1035
Николай Федосеев <xneek@mail.ru>
2016-12-21 09:02:26 +0300

    dsf

---
+++ alarm/serviceworker.js
@@ -0,0 +1,22 @@
+  this.ontask = function(task) {
+        alert(task.data.message);
+        console.log("Task scheduled at: " + new Date(task.time));
+        // From here on we can write the data to IndexedDB, send it to any open windows,
+        // display a notification, etc.
+    }
+
+    // https://example.com/webapp.js
+    function onTaskAdded(task) {
+        console.log("Task successfully scheduled.");
+    }
+
+    function onError(error) {
+        alert("Sorry, couldn't set the alarm: " + error);
+    }
+
+    navigator.serviceWorker.ready.then(function(serviceWorkerRegistration) {
+        serviceWorkerRegistration.taskScheduler.add(Date.now() + (10 * 60000), {
+            message: "It's been 10 minutes, your soup is ready!"
+        }).then(onTaskAdded, onError);
+    });
+        
